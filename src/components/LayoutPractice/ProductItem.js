c4707f63518478786d3c5a2c297d5ace88906d8c
QuanKhs <quanphamluong@gmail.com>
2021-02-07 21:46:07 +0700

    picsum photos

--- src/components/LayoutPractice/ProductItem.js
+++ src/components/LayoutPractice/ProductItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export default class ProductItem extends Component {
     render() {
@@ -7,13 +7,13 @@ export default class ProductItem extends Component {
         return (
             <div className="col-lg-3 col-md-6 mb-4">
                 <div className="card h-100">
-                    <img className="card-img-top" src={item.img} />
+                    <img className="card-img-top" src={item.src} />
                     <div className="card-body">
-                        <h4 className="card-title">{item.title}</h4>
+                        <h4 className="card-title">{item.author}</h4>
                         <p className="card-text">{item.description}</p>
                     </div>
                     <div className="card-footer">
-                        <a href="#" className="btn btn-primary">Find Out More!</a>
+                        <a href={item.link} className="btn btn-primary">Find Out More!</a>
                     </div>
                 </div>
             </div>
