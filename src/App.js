756bac48a811cb1b9bb0825b13a50a21c51fe505
QuanKhs <quanphamluong@gmail.com>
2021-02-22 15:44:59 +0700

    commit master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import LayoutPractice from './components/LayoutPractice/LayoutPractice';
 
 function App() {
   return (
     <div className="App">
-      <LayoutPractice />
     </div>
   );
 }
