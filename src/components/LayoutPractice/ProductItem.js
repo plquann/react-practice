250856f520f2f1e9904137857eb6f31a5382b502
QuanKhs <quanphamluong@gmail.com>
2021-03-07 02:08:29 +0700

    handle data

--- src/components/LayoutPractice/ProductItem.js
+++ src/components/LayoutPractice/ProductItem.js
@@ -7,10 +7,10 @@ export default class ProductItem extends Component {
         return (
             <div className="col-lg-3 col-md-6 mb-4">
                 <div className="card h-100">
-                    <img className="card-img-top" src={item.src} />
+                    <img className="card-img-top h-50" src={item.src} />
                     <div className="card-body">
                         <h4 className="card-title">{item.title}</h4>
-                        <p className="card-text">{item.description}</p>
+                        <p className="card-text">{item.desc}</p>
                     </div>
                     <div className="card-footer">
                         <a href={item.link} className="btn btn-primary">Find Out More!</a>
