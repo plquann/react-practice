5be98e894f14968d15a4da553676ecea1a78ee38
QuanKhs <quanphamluong@gmail.com>
2021-03-07 17:55:17 +0700

    merge 01-02-03 practice

---
+++ src/components/LayoutPractice/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="py-5 bg-dark">
+                    <div className="container">
+                        <p className="m-0 text-center text-white">Copyright © Your Website 2021</p>
+                    </div>
+                </footer>
+            </div>
+        )
+    }
+}
